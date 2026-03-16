49568f6f85dbe16dca1002da2a9bbab211737a25
iguisado228 <ira_gui_lor@goierrieskola.org>
2026-03-16 14:22:29 +0100

    DBa igota eta DB datuak sartzeko inserta ere bai

---
+++ 2Erronka_API/db.sql
@@ -0,0 +1,260 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema 2mg3_2erronka
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema 2mg3_2erronka
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `2mg3_2erronka` DEFAULT CHARACTER SET utf8 ;
+USE `2mg3_2erronka` ;
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`lanpostuak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`lanpostuak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `lanpostua` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`langileak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`langileak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `izena` VARCHAR(20) NOT NULL,
+  `abizena` VARCHAR(45) NOT NULL,
+  `NAN` VARCHAR(9) NOT NULL,
+  `erabiltzaile_izena` VARCHAR(20) NOT NULL,
+  `langile_kodea` INT NOT NULL,
+  `pasahitza` LONGTEXT NOT NULL,
+  `helbidea` VARCHAR(100) NOT NULL,
+  `lanpostuak_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_langileak_lanpostuak1_idx` (`lanpostuak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_langileak_lanpostuak1`
+    FOREIGN KEY (`lanpostuak_id`)
+    REFERENCES `2mg3_2erronka`.`lanpostuak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`mahaiak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`mahaiak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `zenbakia` INT NOT NULL,
+  `pertsona_kopuru` INT NOT NULL,
+  `kokapena` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`erreserbak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`erreserbak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `bezero_izena` VARCHAR(45) NOT NULL,
+  `telefonoa` VARCHAR(9) NOT NULL,
+  `pertsona_kopurua` INT NOT NULL,
+  `eguna_ordua` DATETIME NOT NULL,
+  `prezio_totala` DOUBLE NOT NULL,
+  `faktura_ruta` VARCHAR(45) NULL,
+  `langileak_id` INT NOT NULL,
+  `mahaiak_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `langileak_id`, `mahaiak_id`),
+  INDEX `fk_erreserbak_langileak1_idx` (`langileak_id` ASC) VISIBLE,
+  INDEX `fk_erreserbak_mahaiak1_idx` (`mahaiak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_erreserbak_langileak1`
+    FOREIGN KEY (`langileak_id`)
+    REFERENCES `2mg3_2erronka`.`langileak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_erreserbak_mahaiak1`
+    FOREIGN KEY (`mahaiak_id`)
+    REFERENCES `2mg3_2erronka`.`mahaiak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`eskariak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`eskariak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `prezioa` DOUBLE NOT NULL,
+  `egoera` VARCHAR(45) NOT NULL,
+  `erreserbak_id` INT NOT NULL,
+  `erreserbak_langileak_id` INT NOT NULL,
+  `erreserbak_mahaiak_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_eskariak_erreserbak1_idx` (`erreserbak_id` ASC, `erreserbak_langileak_id` ASC, `erreserbak_mahaiak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_eskariak_erreserbak1`
+    FOREIGN KEY (`erreserbak_id` , `erreserbak_langileak_id` , `erreserbak_mahaiak_id`)
+    REFERENCES `2mg3_2erronka`.`erreserbak` (`id` , `langileak_id` , `mahaiak_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`mota`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`mota` (
+  `id` INT NOT NULL,
+  `izena` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`produktuak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`produktuak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `izena` VARCHAR(60) NOT NULL,
+  `prezioa` DOUBLE NOT NULL,
+  `stock` INT NOT NULL,
+  `mota_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `mota_id`),
+  INDEX `fk_produktuak_mota1_idx` (`mota_id` ASC) VISIBLE,
+  CONSTRAINT `fk_produktuak_mota1`
+    FOREIGN KEY (`mota_id`)
+    REFERENCES `2mg3_2erronka`.`mota` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`hornitzaileak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`hornitzaileak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `izena` VARCHAR(45) NOT NULL,
+  `kontaktua` VARCHAR(50) NOT NULL,
+  `helbidea` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`osagaiak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`osagaiak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `izena` VARCHAR(20) NOT NULL,
+  `prezioa` DOUBLE NOT NULL,
+  `stock` INT NOT NULL,
+  `hornitzaileak_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_osagaiak_hornitzaileak1_idx` (`hornitzaileak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_osagaiak_hornitzaileak1`
+    FOREIGN KEY (`hornitzaileak_id`)
+    REFERENCES `2mg3_2erronka`.`hornitzaileak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`materialak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`materialak` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `izena` VARCHAR(20) NOT NULL,
+  `prezioa` DOUBLE NOT NULL,
+  `stock` INT NOT NULL,
+  `hornitzaileak_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_materialak_hornitzaileak1_idx` (`hornitzaileak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_materialak_hornitzaileak1`
+    FOREIGN KEY (`hornitzaileak_id`)
+    REFERENCES `2mg3_2erronka`.`hornitzaileak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`produktuak_has_osagaiak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`produktuak_has_osagaiak` (
+  `produktuak_id` INT NOT NULL,
+  `osagaiak_id` INT NOT NULL,
+  PRIMARY KEY (`produktuak_id`, `osagaiak_id`),
+  INDEX `fk_produktuak_has_osagaiak_osagaiak1_idx` (`osagaiak_id` ASC) VISIBLE,
+  INDEX `fk_produktuak_has_osagaiak_produktuak1_idx` (`produktuak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_produktuak_has_osagaiak_produktuak1`
+    FOREIGN KEY (`produktuak_id`)
+    REFERENCES `2mg3_2erronka`.`produktuak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_produktuak_has_osagaiak_osagaiak1`
+    FOREIGN KEY (`osagaiak_id`)
+    REFERENCES `2mg3_2erronka`.`osagaiak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`eskariak_has_produktuak`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`eskariak_has_produktuak` (
+  `eskariak_id` INT NOT NULL,
+  `produktuak_id` INT NOT NULL,
+  `kantitatea` INT NOT NULL,
+  `prezioa` DOUBLE NOT NULL,
+  PRIMARY KEY (`eskariak_id`, `produktuak_id`),
+  INDEX `fk_eskariak_has_produktuak_produktuak1_idx` (`produktuak_id` ASC) VISIBLE,
+  INDEX `fk_eskariak_has_produktuak_eskariak1_idx` (`eskariak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_eskariak_has_produktuak_eskariak1`
+    FOREIGN KEY (`eskariak_id`)
+    REFERENCES `2mg3_2erronka`.`eskariak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_eskariak_has_produktuak_produktuak1`
+    FOREIGN KEY (`produktuak_id`)
+    REFERENCES `2mg3_2erronka`.`produktuak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `2mg3_2erronka`.`produktuak_has_osagaiak1`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `2mg3_2erronka`.`produktuak_has_osagaiak1` (
+  `produktuak_id` INT NOT NULL,
+  `osagaiak_id` INT NOT NULL,
+  PRIMARY KEY (`produktuak_id`, `osagaiak_id`),
+  INDEX `fk_produktuak_has_osagaiak1_osagaiak1_idx` (`osagaiak_id` ASC) VISIBLE,
+  INDEX `fk_produktuak_has_osagaiak1_produktuak1_idx` (`produktuak_id` ASC) VISIBLE,
+  CONSTRAINT `fk_produktuak_has_osagaiak1_produktuak1`
+    FOREIGN KEY (`produktuak_id`)
+    REFERENCES `2mg3_2erronka`.`produktuak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_produktuak_has_osagaiak1_osagaiak1`
+    FOREIGN KEY (`osagaiak_id`)
+    REFERENCES `2mg3_2erronka`.`osagaiak` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ 2Erronka_API/inserts_euskaraz.sql
@@ -0,0 +1,91 @@
+USE `2mg3_2erronka`;
+
+-- Kanpo gakoen egiaztapena desgaitu datuak sartu bitartean, ordena arazoak ekiditeko
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Taulak garbitu (aukerakoa, baina gomendagarria datu garbiak izateko)
+TRUNCATE TABLE `eskariak_has_produktuak`;
+TRUNCATE TABLE `produktuak_has_osagaiak`;
+TRUNCATE TABLE `eskariak`;
+TRUNCATE TABLE `erreserbak`;
+TRUNCATE TABLE `osagaiak`;
+TRUNCATE TABLE `produktuak`;
+TRUNCATE TABLE `mota`;
+TRUNCATE TABLE `mahaiak`;
+TRUNCATE TABLE `langileak`;
+TRUNCATE TABLE `hornitzaileak`;
+TRUNCATE TABLE `lanpostuak`;
+
+-- 1. Lanpostuak
+INSERT INTO `lanpostuak` (`id`, `lanpostua`) VALUES 
+(1, 'Gerentea'),
+(2, 'Zerbitzaria'),
+(3, 'Sukaldaria'),
+(4, 'Administratzailea');
+
+-- 2. Hornitzaileak
+INSERT INTO `hornitzaileak` (`id`, `izena`, `kontaktua`, `helbidea`) VALUES 
+(1, 'Eroski Banaketa', '944123456', 'Elorrio Kalea 10, Bizkaia'),
+(2, 'Makro Elikagaiak', '943987654', 'Oiartzun Industrialdea, Gipuzkoa'),
+(3, 'Harategi Berria', '945555666', 'Gasteizko Plaza, Araba');
+
+-- 3. Mota (Produktuentzat)
+INSERT INTO `mota` (`id`, `izena`) VALUES 
+(1, 'Edaria'),
+(2, 'Janaria'),
+(3, 'Postrea');
+
+-- 4. Mahaiak
+INSERT INTO `mahaiak` (`id`, `zenbakia`, `pertsona_kopuru`, `kokapena`) VALUES 
+(1, 1, 4, 'Egongela Nagusia'),
+(2, 2, 2, 'Terraza'),
+(3, 3, 6, 'Leiho ondoan'),
+(4, 4, 4, 'Sukalde ondoan');
+
+-- 5. Langileak (Pasahitza: "1234" SHA256 hasha)
+INSERT INTO `langileak` (`id`, `izena`, `abizena`, `NAN`, `erabiltzaile_izena`, `langile_kodea`, `pasahitza`, `helbidea`, `lanpostuak_id`) VALUES 
+(1, 'Ane', 'Arrieta', '12345678A', 'ane1', 101, '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'Kale Nagusia 1, Bilbo', 1),
+(2, 'Jon', 'Mendizabal', '87654321B', 'jon2', 102, '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'Araba Kalea 5, Donostia', 2),
+(3, 'Miren', 'Lasa', '11223344C', 'miren3', 103, '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'Gasteiz Hiribidea 12, Gasteiz', 3);
+
+-- 6. Produktuak
+INSERT INTO `produktuak` (`id`, `izena`, `prezioa`, `stock`, `mota_id`) VALUES 
+(1, 'Coca-Cola', 2.50, 50, 1),
+(2, 'Kaiku Esnea', 1.20, 100, 1),
+(3, 'Tortilla Pintxoa', 1.80, 20, 2),
+(4, 'Bakailaoa Bizkaitar Erara', 15.50, 15, 2),
+(5, 'Gazta Tarta', 4.50, 10, 3);
+
+-- 7. Osagaiak
+INSERT INTO `osagaiak` (`id`, `izena`, `prezioa`, `stock`, `hornitzaileak_id`) VALUES 
+(1, 'Patata', 0.50, 200, 1),
+(2, 'Arrautza', 0.20, 150, 2),
+(3, 'Bakailao Freskoa', 8.00, 30, 3),
+(4, 'Gazta Krematsua', 3.50, 20, 1),
+(5, 'Coca-Cola botila', 1.00, 100, 1); -- Produktu bezala saltzen dena, baina osagai/stock bezala kudeatua
+
+-- 8. Erreserbak
+INSERT INTO `erreserbak` (`id`, `bezero_izena`, `telefonoa`, `pertsona_kopurua`, `eguna_ordua`, `prezio_totala`, `faktura_ruta`, `langileak_id`, `mahaiak_id`) VALUES 
+(1, 'Gorka Urkizu', '600111222', 4, '2026-03-20 14:30:00', 45.50, 'tiketak/tiket_1.pdf', 1, 1),
+(2, 'Idurre Lopez', '655444333', 2, '2026-03-21 21:00:00', 25.00, NULL, 2, 2);
+
+-- 9. Eskariak
+INSERT INTO `eskariak` (`id`, `prezioa`, `egoera`, `erreserbak_id`, `erreserbak_langileak_id`, `erreserbak_mahaiak_id`) VALUES 
+(1, 18.50, 'Bukatua', 1, 1, 1),
+(2, 12.00, 'Prestatzen', 2, 2, 2);
+
+-- 10. Produktuak eta Osagaiak harremana
+INSERT INTO `produktuak_has_osagaiak` (`produktuak_id`, `osagaiak_id`) VALUES 
+(1, 5), -- Coca-Cola (produktua) -> Coca-Cola botila (osagaia)
+(3, 1),
+(3, 2),
+(4, 3);
+
+-- 11. Eskariak eta Produktuak harremana
+INSERT INTO `eskariak_has_produktuak` (`eskariak_id`, `produktuak_id`, `kantitatea`, `prezioa`) VALUES 
+(1, 1, 2, 5.00),
+(1, 3, 2, 3.60),
+(2, 4, 1, 15.50);
+
+-- Egiaztapena berriro gaitu
+SET FOREIGN_KEY_CHECKS = 1;
